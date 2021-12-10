e5f067c15ad45c85d5d85b7ba220f826cfb507cd
samuelmmorse <samuel.morse@maine.edu>
2021-12-10 01:48:30 -0500

    modeling

--- src/median_housing_price_tensorflow.py
+++ src/median_housing_price_tensorflow.py
@@ -1,11 +1,19 @@
 import os
-
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import os
+from math import sqrt
+import pandas as pd #type: ignore
+import numpy as np #type: ignore
+from numpy import concatenate #type: ignore
+import matplotlib.pyplot as plt #type: ignore
+import tensorflow as tf #type: ignore
+from pandas import DataFrame #type: ignore
+from pandas import concat #type: ignore
+from sklearn.preprocessing import LabelEncoder #type: ignore
+from sklearn.preprocessing import MinMaxScaler #type: ignore
+from sklearn.metrics import mean_squared_error #type: ignore
+from keras.models import Sequential #type: ignore
+from keras.layers import Dense #type: ignore
+from keras.layers import LSTM #type: ignore
+from keras import activations as activ # type: ignore
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -24,117 +32,117 @@ mhp_df = pd.read_csv(
 )
 mhp_df.head()
 
-train_df, test_df = mhp_df[1:15000], mhp_df[15000:]
 
-train = train_df
-scalers = {}
-for i in train_df.columns:
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    s_s = scaler.fit_transform(train[i].values.reshape(-1, 1))
-    s_s = np.reshape(s_s, len(s_s))
-    scalers["scaler_" + i] = scaler
-    train[i] = s_s
-test = test_df
-for i in train_df.columns:
-    scaler = scalers["scaler_" + i]
-    s_s = scaler.transform(test[i].values.reshape(-1, 1))
-    s_s = np.reshape(s_s, len(s_s))
-    scalers["scaler_" + i] = scaler
-    test[i] = s_s
+print(mhp_df.head(5))
 
-
-def split_series(series, n_past, n_future):
-    #
-    # n_past ==> no of past observations
-    #
-    # n_future ==> no of future observations
-    #
-    X, y = list(), list()
-    for window_start in range(len(series)):
-        past_end = window_start + n_past
-        future_end = past_end + n_future
-        if future_end > len(series):
-            break
-        # slicing the past and future parts of the window
-        past, future = series[window_start:past_end, :], series[past_end:future_end, :]
-        X.append(past)
-        y.append(future)
-    return np.array(X), np.array(y)
+# values = mhp_df.values
+# # specify columns to plot
+# groups = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# i = 1
+# # plot each column
+# plt.figure()
+# plt.subplots_adjust(left= .125, right= .9, bottom= .1, top= 1.9, wspace=.2, hspace=.2)
+# plt.rcParams['font.size'] = 8
+# for group in groups:
+# 	plt.subplot(len(groups), 1, i)
+# 	plt.plot(values[:, group])
+# 	plt.title(mhp_df.columns[group], y=.05, loc='right')
+# 	i += 1
+# plt.show()
 
 
-n_past = 730
-n_future = 365
+# convert series to supervised learning
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+
+
+# Load in the data set
+values = mhp_df.values
+# integer encode direction
+encoder = LabelEncoder()
+values[:,4] = encoder.fit_transform(values[:,4])
+# ensure all data is float
+values = values.astype('float32')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+# specify the number of lag days
+n_days = 365 * 3
 n_features = 11
+# frame as supervised learning
+reframed = series_to_supervised(scaled, n_days, 1)
+print(reframed.shape)
 
 
-X_train, y_train = split_series(train.values, n_past, n_future)
-X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], n_features))
-y_train = y_train.reshape((y_train.shape[0], y_train.shape[1], n_features))
-X_test, y_test = split_series(test.values, n_past, n_future)
-X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], n_features))
-y_test = y_test.reshape((y_test.shape[0], y_test.shape[1], n_features))
+# split into train and test sets
+values = reframed.values
+n_train_days = 17000
+train = values[:n_train_days, :]
+test = values[n_train_days:, :]
+# split into input and outputs
+n_obs = n_days * n_features
+train_X, train_y = train[:, :n_obs], train[:, -n_features]
+test_X, test_y = test[:, :n_obs], test[:, -n_features]
+print(train_X.shape, len(train_X), train_y.shape)
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], n_days, n_features))
+test_X = test_X.reshape((test_X.shape[0], n_days, n_features))
+# print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
 
-
-# E2D2
-# n_features ==> no of features at each timestep in the data.
-#
-encoder_inputs = tf.keras.layers.Input(shape=(n_past, n_features))
-encoder_l1 = tf.keras.layers.LSTM(100, return_sequences=True, return_state=True)
-encoder_outputs1 = encoder_l1(encoder_inputs)
-encoder_states1 = encoder_outputs1[1:]
-encoder_l2 = tf.keras.layers.LSTM(100, return_state=True)
-encoder_outputs2 = encoder_l2(encoder_outputs1[0])
-encoder_states2 = encoder_outputs2[1:]
-#
-decoder_inputs = tf.keras.layers.RepeatVector(n_future)(encoder_outputs2[0])
-#
-decoder_l1 = tf.keras.layers.LSTM(100, return_sequences=True)(
-    decoder_inputs, initial_state=encoder_states1
-)
-decoder_l2 = tf.keras.layers.LSTM(100, return_sequences=True)(
-    decoder_l1, initial_state=encoder_states2
-)
-decoder_outputs2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(n_features))(
-    decoder_l2
-)
-#
-model_e2d2 = tf.keras.models.Model(encoder_inputs, decoder_outputs2)
-#
-model_e2d2.summary()
+# design network
+model = Sequential()
+model.add(LSTM(200, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1, activation=activ.relu))
+model.add(Dense(16, activation=activ.relu))
+model.add(Dense(64, activation=activ.relu))
+model.add(Dense(128, activation=activ.relu))
+model.add(Dense(64, activation=activ.relu))
+model.add(Dense(32, activation=activ.relu))
+model.add(Dense(16, activation=activ.relu))
+model.add(Dense(1, activation=activ.relu))
+model.compile(loss='mae', optimizer='adam')
+# fit network
+history = model.fit(train_X, train_y, epochs=50, batch_size=128, validation_data=(test_X, test_y), verbose=1, shuffle=False)
+# plot history
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+plt.show()
 
 
-reduce_lr = tf.keras.callbacks.LearningRateScheduler(lambda x: 1e-3 * 0.90 ** x)
-
-model_e2d2.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.Huber())
-history_e2d2 = model_e2d2.fit(
-    X_train,
-    y_train,
-    epochs=25,
-    validation_data=(X_test, y_test),
-    batch_size=32,
-    verbose=1,
-    callbacks=[reduce_lr],
-)
-
-
-
-pred_e2d2 = model_e2d2.predict(X_test)
-pred1_e2d2 = model_e2d2.predict(y_test)
-
-
-for index,i in enumerate(train_df.columns):
-    scaler = scalers['scaler_'+i]
-    pred1_e2d2[:,:,index]=scaler.inverse_transform(pred1_e2d2[:,:,index])
-    pred_e2d2[:,:,index]=scaler.inverse_transform(pred_e2d2[:,:,index])
-    y_train[:,:,index]=scaler.inverse_transform(y_train[:,:,index])
-    y_test[:,:,index]=scaler.inverse_transform(y_test[:,:,index])
-
-from sklearn.metrics import mean_absolute_error
-for index,i in enumerate(train_df.columns):
-  print(i)
-  for j in range(1,6):
-    print("Day ",j,":")
-    print("MAE-E2D2 : ",mean_absolute_error(y_test[:,j-1,index],pred1_e2d2[:,j-1,index]))
-  print()
-  print()
+# make a prediction
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], n_days*n_features))
+# invert scaling for forecast
+inv_yhat = concatenate((yhat, test_X[:, -10:]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = concatenate((test_y, test_X[:, -10:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+# calculate RMSE
+rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test RMSE: %.3f' % rmse)
---
+++ src/median_housing_price_tensorflow_nanloss_example.py
@@ -0,0 +1,156 @@
+import os
+
+import pandas as pd #type: ignore
+import numpy as np #type: ignore
+from sklearn.preprocessing import MinMaxScaler #type: ignore
+import matplotlib.pyplot as plt #type: ignore
+import tensorflow as tf #type: ignore
+from keras import activations as activ # type: ignore
+import os
+
+pd.options.mode.chained_assignment = None  # default='warn'
+
+dir_name = os.path.dirname(__file__)
+relative_path = "data/train.csv"
+file_path = os.path.join(dir_name, relative_path)
+
+mhp_df = pd.read_csv(
+    file_path,
+    sep=",",
+    header=0,
+    low_memory=False,
+    infer_datetime_format=True,
+    parse_dates={"datetime": [0]},
+    index_col=["datetime"],
+)
+mhp_df.head()
+
+train_df, test_df = mhp_df[1:15000], mhp_df[15000:]
+
+train = train_df
+scalers = {}
+for i in train_df.columns:
+    scaler = MinMaxScaler(feature_range=(0, 100))
+    s_s = scaler.fit_transform(train[i].values.reshape(-1, 1))
+    s_s = np.reshape(s_s, len(s_s))
+    scalers["scaler_" + i] = scaler
+    train[i] = s_s
+test = test_df
+for i in train_df.columns:
+    scaler = scalers["scaler_" + i]
+    s_s = scaler.transform(test[i].values.reshape(-1, 1))
+    s_s = np.reshape(s_s, len(s_s))
+    scalers["scaler_" + i] = scaler
+    test[i] = s_s
+
+
+def split_series(series, n_past, n_future):
+    #
+    # n_past ==> no of past observations
+    #
+    # n_future ==> no of future observations
+    #
+    X, y = list(), list()
+    for window_start in range(len(series)):
+        past_end = window_start + n_past
+        future_end = past_end + n_future
+        if future_end > len(series):
+            break
+        # slicing the past and future parts of the window
+        past, future = series[window_start:past_end, :], series[past_end:future_end, :]
+        X.append(past)
+        y.append(future)
+    return np.array(X), np.array(y)
+
+
+n_past = 1095
+n_future = 365
+n_features = 11
+
+
+X_train, y_train = split_series(train.values, n_past, n_future)
+X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], n_features))
+y_train = y_train.reshape((y_train.shape[0], y_train.shape[1], n_features))
+X_test, y_test = split_series(test.values, n_past, n_future)
+X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], n_features))
+y_test = y_test.reshape((y_test.shape[0], y_test.shape[1], n_features))
+
+
+# E2D2
+# n_features ==> no of features at each timestep in the data.
+#
+encoder_inputs = tf.keras.layers.Input(shape=(n_past, n_features))
+encoder_l1 = tf.keras.layers.LSTM(100, return_sequences=True, return_state=True)
+encoder_outputs1 = encoder_l1(encoder_inputs)
+encoder_states1 = encoder_outputs1[1:]
+encoder_l2 = tf.keras.layers.LSTM(100, return_state=True)
+encoder_outputs2 = encoder_l2(encoder_outputs1[0])
+encoder_states2 = encoder_outputs2[1:]
+#
+decoder_inputs = tf.keras.layers.RepeatVector(n_future)(encoder_outputs2[0])
+#
+decoder_l1 = tf.keras.layers.LSTM(100, return_sequences=True)(
+    decoder_inputs, initial_state=encoder_states1
+)
+denseLayer1 = tf.keras.layers.Dense(64, activation=activ.relu)
+decoder_l2 = tf.keras.layers.LSTM(100, return_sequences=True)(
+    decoder_l1, initial_state=encoder_states2
+)
+decoder_outputs2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(n_features))(
+    decoder_l2
+)
+#
+model_e2d2 = tf.keras.models.Model(encoder_inputs, decoder_outputs2)
+#
+model_e2d2.summary()
+
+
+plt.plot(train)
+plt.show()
+
+
+reduce_lr = tf.keras.callbacks.LearningRateScheduler(lambda x: 1e-3 * 0.001 ** x)
+
+# callback = tf.keras.callbacks.EarlyStopping(
+#     monitor='val_loss', min_delta=0, patience=0, verbose=0,
+#     mode='auto', baseline=None, restore_best_weights=False
+# )
+
+model_e2d2.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=1e-15, clipnorm=1, clipvalue=1),
+    loss=tf.keras.losses.Huber(),
+)
+history_e2d2 = model_e2d2.fit(
+    X_train,
+    y_train,
+    epochs=5,
+    validation_data=(X_test, y_test),
+    batch_size=128,
+    verbose=1,
+    callbacks=[reduce_lr]
+)
+
+
+pred_e2d2 = model_e2d2.predict(X_test)
+pred1_e2d2 = model_e2d2.predict(y_test)
+
+
+for index, i in enumerate(train_df.columns):
+    scaler = scalers["scaler_" + i]
+    pred1_e2d2[:, :, index] = scaler.inverse_transform(pred1_e2d2[:, :, index])
+    pred_e2d2[:, :, index] = scaler.inverse_transform(pred_e2d2[:, :, index])
+    y_train[:, :, index] = scaler.inverse_transform(y_train[:, :, index])
+    y_test[:, :, index] = scaler.inverse_transform(y_test[:, :, index])
+
+from sklearn.metrics import mean_absolute_error #type: ignore
+
+for index, i in enumerate(train_df.columns):
+    print(i)
+    for j in range(1, 6):
+        print("Day ", j, ":")
+        print(
+            "MAE-E2D2 : ",
+            mean_absolute_error(y_test[:, j - 1, index], pred1_e2d2[:, j - 1, index]),
+        )
+    print()
+    print()
